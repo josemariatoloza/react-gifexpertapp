166286ca36ced0f67e6c09545506b00e6bbfc89a
Jose <tolozajosemaria@gmail.com>
2020-07-26 17:33:37 +0200

    docs agregado

---
+++ docs/precache-manifest.ee6f81ed123fb885d8325101e564973d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b91c9a3d137a4cf4bddd3e8bddeb9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c6c9ef72b3356b051f",
+    "url": "/static/css/main.39ef9c8d.chunk.css"
+  },
+  {
+    "revision": "1613a5c9dc3b943fd476",
+    "url": "/static/js/2.06100df6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06100df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26c6c9ef72b3356b051f",
+    "url": "/static/js/main.bff75242.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
